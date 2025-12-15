ae16a7fe1836d103d52e3f28c53d899137aa892c
Whitemehat <wong1234.pe@gmail.com>
2025-12-15 17:45:22 +0700

    Final real

--- src/q16/ValidatePass.ts
+++ src/q16/ValidatePass.ts
@@ -7,7 +7,7 @@ if(input.length >= 8){
     for(let i = 0;i<input.length;i++){
         if(input.charCodeAt(i) >= 65 && input.charCodeAt(i) <= 90)have_u=true;
         if(!isNaN(Number(input[i])))have_n=true;
-        if(input.charCodeAt(i) >= 97 && input.charCodeAt(i) <= 122)have_u=true;
+        if(input.charCodeAt(i) >= 97 && input.charCodeAt(i) <= 122)have_l=true;
     }
 }
 if(have_u && have_n && have_l){
